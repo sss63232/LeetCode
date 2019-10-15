1384b3509b5f19624a83637614c9850145664247
YU-HSIN, CHEN <s63232@gmail.com>
2019-10-15 22:41:23 +0800

    feat: Add 20

---
+++ 20.valid-parentheses.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s === '') {
+    return true
+  }
+
+  const leftBrackets = ['(', '{', '[']
+  const rightBrackets = [')', '}', ']']
+
+  const isMatched = (a, b) =>
+    leftBrackets.indexOf(a) === rightBrackets.indexOf(b)
+
+  const foundLeftBrackets = []
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i]
+    if (leftBrackets.indexOf(char) >= 0) {
+      foundLeftBrackets.push(char)
+    } else if (rightBrackets.indexOf(char) >= 0) {
+      if (!isMatched(foundLeftBrackets.pop(), char)) {
+        return false
+      }
+    }
+  }
+
+  return foundLeftBrackets.length === 0
+}
+// @lc code=end
