c4fab9557718fbc077b922d00247bb4df2b73c16
YU-HSIN, CHEN <s63232@gmail.com>
2019-11-20 21:28:27 +0800

    feat: Update 63

--- 63.unique-paths-ii.js
+++ 63.unique-paths-ii.js
@@ -54,38 +54,18 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function (obstacleGrid) {
-  const m = obstacleGrid[0].length
-  const n = obstacleGrid.length
-
-  if (obstacleGrid[0][0] === 1 || obstacleGrid[n - 1][m - 1] === 1) {
-    return 0
-  }
-
-  const ans = new Array(n).fill(0).map(() => new Array(m).fill(0))
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
+  for (let i = 0; i < obstacleGrid.length; i++) {
+    for (let j = 0; j < obstacleGrid[0].length; j++) {
       if (obstacleGrid[i][j] === 1) {
-        ans[i][j] = 0
-        continue
+        obstacleGrid[i][j] = 0
+      } else if (i === 0 && j === 0) {
+        obstacleGrid[i][j] = 1
+      } else {
+        obstacleGrid[i][j] = obstacleGrid[i][j - 1 >= 0 ? j - 1 : 0] + obstacleGrid[i - 1 >= 0 ? i - 1 : 0][j]
       }
-
-      if (i === 0 && j === 0) {
-        ans[i][j] = 1
-        continue
-      }
-
-      const upperValue = i - 1 >= 0
-        ? ans[i - 1][j]
-        : 0
-      const leftValue = j - 1 >= 0
-        ? ans[i][j - 1]
-        : 0
-
-      ans[i][j] = upperValue + leftValue
     }
   }
 
-  return ans[n - 1][m - 1]
+  return obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1]
 }
 // @lc code=end
