f1eb95453a0616307cbfcb5eb4c1ee3c4a24d996
YU-HSIN, CHEN <s63232@gmail.com>
2019-12-11 22:16:49 +0800

    feat: Add 704

---
+++ 704.binary-search.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=704 lang=javascript
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (49.81%)
+ * Likes:    388
+ * Dislikes: 36
+ * Total Accepted:    87.1K
+ * Total Submissions: 174.4K
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given a sorted (in ascending order) integer array nums of n elements and a
+ * target value, write a function to search target in nums. If target exists,
+ * then return its index, otherwise return -1.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  const _searchTargetWithinNums = (left, right) => {
+    if (right - left < 0) {
+      return -1
+    }
+    const middleIdx = left + Math.floor((right - left) / 2)
+    const middleNum = nums[middleIdx]
+
+    if (middleNum === target) {
+      return middleIdx
+    }
+
+    if (target > middleNum) {
+      return _searchTargetWithinNums(middleIdx + 1, right)
+    } else {
+      return _searchTargetWithinNums(left, middleIdx - 1)
+    }
+  }
+
+  return _searchTargetWithinNums(0, nums.length - 1)
+}
+// @lc code=end
