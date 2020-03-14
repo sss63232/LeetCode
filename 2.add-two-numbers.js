4e40b3c9a45b3dafc7e55b1546894b991fc78d40
Newt Chen <yu-hsin.chen@verizonmedia.com>
2020-03-14 17:18:35 +0800

    feat(add-two-sum): Add add-two-sum

---
+++ 2.add-two-numbers.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=2 lang=javascript
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (32.86%)
+ * Likes:    7251
+ * Dislikes: 1883
+ * Total Accepted:    1.2M
+ * Total Submissions: 3.8M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
+ * Example:
+ *
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let resultNode = new ListNode(0)
+
+  let currentNode = resultNode
+  let carry = 0
+  while (
+    l1 !== null ||
+    l2 !== null ||
+    carry > 0) {
+    let sum = 0
+
+    if (l1 !== null) {
+      sum += l1.val
+      l1 = l1.next
+    }
+
+    if (l2 !== null) {
+      sum += l2.val
+      l2 = l2.next
+    }
+
+    sum += carry
+
+    currentNode.next = new ListNode(sum % 10)
+    currentNode = currentNode.next
+
+    carry = Math.floor(sum / 10)
+  }
+
+  return resultNode.next
+}
+// @lc code=end
