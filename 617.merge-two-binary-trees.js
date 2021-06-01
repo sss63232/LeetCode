a7ce235deda2691883f10244b091c3ac00b726a8
8400_WIN10_PRO\s6323 <s63232@gmail.com>
2021-06-01 09:57:38 +0800

    feat: 617 iteration

--- 617.merge-two-binary-trees.js
+++ 617.merge-two-binary-trees.js
@@ -65,13 +65,20 @@
  * @return {TreeNode}
  */
 const mergeTrees = function (root1, root2) {
-  if (!root1) return root2
-  if (!root2) return root1
+  if (!root1 || !root2) return root1 || root2
 
-  return new TreeNode(
-    root1.val + root2.val,
-    mergeTrees(root1.left, root2.left),
-    mergeTrees(root1.right, root2.right)
-  )
+  const queue = [[root1, root2]]
+  while (queue.length) {
+    const [node1, node2] = queue.shift()
+    node1.val += node2.val
+
+    node1.left && node2.left && queue.push([node1.left, node2.left])
+    node1.right && node2.right && queue.push([node1.right, node2.right])
+
+    if (!node1.left) node1.left = node2.left
+    if (!node1.right) node1.right = node2.right
+  }
+
+  return root1
 }
 // @lc code=end
