f258655066d3cfd1c90b344e7147e33a95422400
8400_WIN10_PRO\s6323 <s63232@gmail.com>
2021-06-01 10:36:42 +0800

    feat(654): Add 654

---
+++ 654.maximum-binary-tree.js
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode id=654 lang=javascript
+ *
+ * [654] Maximum Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (81.13%)
+ * Likes:    2586
+ * Dislikes: 274
+ * Total Accepted:    167.4K
+ * Total Submissions: 205K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * You are given an integer array nums with no duplicates. A maximum binary
+ * tree can be built recursively from nums using the following algorithm:
+ *
+ *
+ * Create a root node whose value is the maximum value in nums.
+ * Recursively build the left subtree on the subarray prefix to the left of the
+ * maximum value.
+ * Recursively build the right subtree on the subarray suffix to the right of
+ * the maximum value.
+ *
+ *
+ * Return the maximum binary tree built from nums.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [3,2,1,6,0,5]
+ * Output: [6,3,5,null,2,0,null,null,1]
+ * Explanation: The recursive calls are as follow:
+ * - The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right
+ * suffix is [0,5].
+ * ⁠   - The largest value in [3,2,1] is 3. Left prefix is [] and right suffix
+ * is [2,1].
+ * ⁠       - Empty array, so no child.
+ * ⁠       - The largest value in [2,1] is 2. Left prefix is [] and right
+ * suffix is [1].
+ * ⁠           - Empty array, so no child.
+ * ⁠           - Only one element, so child is a node with value 1.
+ * ⁠   - The largest value in [0,5] is 5. Left prefix is [0] and right suffix
+ * is [].
+ * ⁠       - Only one element, so child is a node with value 0.
+ * ⁠       - Empty array, so no child.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [3,2,1]
+ * Output: [3,null,2,null,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * All integers in nums are unique.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+const constructMaximumBinaryTree = function (nums) {
+  if (!nums.length) return null
+  if (nums.length === 1) return new TreeNode(nums[0])
+
+  let maxNumIdx = 0
+  for (let i = 1; i < nums.length; i++) {
+    maxNumIdx = nums[i] > nums[maxNumIdx] ? i : maxNumIdx
+  }
+
+  return new TreeNode(
+    nums[maxNumIdx],
+    constructMaximumBinaryTree(nums.slice(0, maxNumIdx)),
+    constructMaximumBinaryTree(nums.slice(maxNumIdx + 1))
+  )
+}
+// @lc code=end
