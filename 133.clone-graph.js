dcdb2947801c71c2d9ca18fa4215f2bb2d2d9ccd
yuhsin.chen <s63232@gmail.com>
2021-06-12 18:50:04 +0800

    feat: Update

--- 133.clone-graph.js
+++ 133.clone-graph.js
@@ -14,38 +14,38 @@
  * Testcase Example:  '[[2,4],[1,3],[2,4],[1,3]]'
  *
  * Given a reference of a node in a connected undirected graph.
- * 
+ *
  * Return a deep copy (clone) of the graph.
- * 
+ *
  * Each node in the graph contains a val (int) and a list (List[Node]) of its
  * neighbors.
- * 
- * 
+ *
+ *
  * class Node {
  * ⁠   public int val;
  * ⁠   public List<Node> neighbors;
  * }
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Test case format:
- * 
+ *
  * For simplicity sake, each node's value is the same as the node's index
  * (1-indexed). For example, the first node with val = 1, the second node with
  * val = 2, and so on. The graph is represented in the test case using an
  * adjacency list.
- * 
+ *
  * Adjacency list is a collection of unordered lists used to represent a finite
  * graph. Each list describes the set of neighbors of a node in the graph.
- * 
+ *
  * The given node will always be the first node with val = 1. You must return
  * the copy of the given node as a reference to the cloned graph.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
  * Output: [[2,4],[1,3],[2,4],[1,3]]
  * Explanation: There are 4 nodes in the graph.
@@ -57,44 +57,44 @@
  * 4).
  * 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val =
  * 3).
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: adjList = [[]]
  * Output: [[]]
  * Explanation: Note that the input contains one empty list. The graph consists
  * of only one node with val = 1 and it does not have any neighbors.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: adjList = []
  * Output: []
  * Explanation: This an empty graph, it does not have any nodes.
- * 
- * 
+ *
+ *
  * Example 4:
- * 
- * 
+ *
+ *
  * Input: adjList = [[2],[1]]
  * Output: [[2],[1]]
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= Node.val <= 100
  * Node.val is unique for each node.
  * Number of Nodes will not exceed 100.
  * There is no repeated edges and no self-loops in the graph.
  * The Graph is connected and all nodes can be visited starting from the given
  * node.
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -110,29 +110,28 @@
  * @param {Node} node
  * @return {Node}
  */
-var cloneGraph = function (node) {
-    if (!node) return
+const cloneGraph = function (node) {
+  if (!node) return null
 
-    const discovered = {
-        [node.val]: new Node(node.val)
-    }
+  const clones = []
+  const visited = new Set()
 
-    const stack = [node]
-    while (stack.length) {
-        const cur = stack.pop()
-        const cloneOfCur = discovered[cur.val]
+  const stack = [node]
+  while (stack.length) {
+    const cur = stack.pop()
+    visited.add(cur.val)
 
-        cur.neighbors.forEach(n => {
-            if (!discovered[n.val]) {
-                discovered[n.val] = new Node(n.val)
-                stack.push(n)
-            }
-            cloneOfCur.neighbors.push(discovered[n.val])
-        })
+    clones[cur.val] = clones[cur.val] || new Node(cur.val)
 
-    }
+    cur.neighbors.forEach(neighbor => {
+      clones[neighbor.val] = clones[neighbor.val] || new Node(neighbor.val)
 
-    return discovered[node.val]
-};
+      if (!visited.has(neighbor.val)) stack.push(neighbor)
+    })
+
+    clones[cur.val].neighbors = cur.neighbors.map(neighbor => clones[neighbor.val])
+  }
+
+  return clones[1]
+}
 // @lc code=end
-
