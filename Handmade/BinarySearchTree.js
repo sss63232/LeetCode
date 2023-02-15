fc4e4c8e849ffbb1f1c5f56b531e2c51d7b26ff2
yu-hsin.chen <yu-hsin.chen@yu-hsin.local>
2023-02-16 01:11:53 +0900

    Add min, max, and has methods to BinarySearchTree.js

--- Handmade/BinarySearchTree.js
+++ Handmade/BinarySearchTree.js
@@ -71,21 +71,69 @@ class BinarySearchTree {
       callback(treeRoot.key)
     }
   }
+
+  min() {
+    return this.findMinFrom(this.root)
+  }
+
+  findMinFrom(rootNode) {
+    if (!rootNode) {
+      return null
+    }
+
+    let current = rootNode
+    while (current.left) {
+      current = current.left
+    }
+    return current.key
+  }
+
+  max() {
+    return this.findMaxBy(this.root)
+  }
+
+  findMaxBy(rootNode) {
+    if (!rootNode) {
+      return null
+    }
+
+    let current = rootNode
+    while (current.right) {
+      current = current.right
+    }
+    return current.key
+  }
+
+  has(key) {
+    return this.hasNode(this.root, key)
+  }
+
+  hasNode(rootNode, key) {
+    if (!rootNode) {
+      return false
+    }
+
+    if (key === rootNode.key) {
+      return true
+    }
+
+    if (key > rootNode.key) {
+      return this.hasNode(rootNode.right, key)
+    } else {
+      return this.hasNode(rootNode.left, key)
+    }
+  }
 }
 
 const tree = new BinarySearchTree()
 tree.insert(3)
 tree.insert(5)
+tree.insert(99)
 tree.insert(1)
+tree.insert(0)
 tree.insert(9)
 tree.insert(7)
-tree.inOrderTraverse(console.log)
-console.log("TCL=> ~ ⬆️inOrderTraverse")
-tree.preOrderTraverse(console.log)
-console.log("TCL=> ~ ⬆️preOrderTraverse")
-tree.postOrderTraverse(console.log)
-console.log("TCL=> ~ ⬆️postOrderTraverse")
-
-3
-1, 5
-7, 9
+console.log("TCL=> ~ tree.min()", tree.min())
+console.log("TCL=> ~ tree.max()", tree.max())
+console.log("TCL=> ~ tree.has(100)", tree.has(100))
+console.log("TCL=> ~ tree.has(9)", tree.has(9))
